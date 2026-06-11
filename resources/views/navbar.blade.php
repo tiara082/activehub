75b7eaf674d487947f9a26ad85fd06d321f2515f
tiara082 <tiarafebrianie308@gmail.com>
2026-06-11 14:44:41 +0700

    fix: besarkan tulisan ActiveHub jadi text-2xl

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,7 +4,7 @@
     {{-- LOGO --}}
     <a href="/" class="flex items-center gap-2">
         <img src="{{ asset('assets/logo yellow.png') }}" alt="ActiveHub" class="h-8 w-auto">
-        <span class="font-display text-xl font-black text-yellow-300 tracking-tight leading-none">ActiveHub</span>
+        <span class="font-display text-2xl font-black text-yellow-300 tracking-tight leading-none">ActiveHub</span>
     </a>
 
     {{-- DESKTOP MENU --}}
